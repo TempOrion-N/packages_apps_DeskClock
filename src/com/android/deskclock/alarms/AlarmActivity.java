329305f54d4b32c2f350f2bb826ade00cac59a4e
Danesh M <daneshm90@gmail.com>
2016-02-23 11:04:08 -0800

    DeskClock : Don't set POWER_OFF_ALARM_MODE unecessarily
    Setting this even if alarm is cancelled can get the user in a state
    where the flag is never unset, and hence consume their home/search/menu keys...etc.
    
    CYNGNOS-2119
    
    Change-Id: I7b16de8530c937192d2c87b0ae52438185eca8d8

--- src/com/android/deskclock/alarms/AlarmActivity.java
+++ src/com/android/deskclock/alarms/AlarmActivity.java
@@ -204,8 +204,7 @@ public class AlarmActivity extends AppCompatActivity
         if (mIsPowerOffAlarm) {
             // if we are invoked by a power off alarm, we may have just missed the alarm
             // time if the boot took unusually long (we normally wake up two minutes early)
-
-            Settings.System.putInt(mContext.getContentResolver(), POWER_OFF_ALARM_MODE, 1);
+            mAlarmInstance = AlarmInstance.getFirstMissedInstance(mContext.getContentResolver());
         } else if (intentData != null) {
             long instanceId = AlarmInstance.getId(intentData);
             mAlarmInstance = AlarmInstance.getInstance(this.getContentResolver(), instanceId);
